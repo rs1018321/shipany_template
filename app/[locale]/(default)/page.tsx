5f64cd04e58b620235330d699a6e3e9fec86af0c
rs1018321 <ivorycannon14102001jdu@gmail.com>
2025-05-30 17:54:47 +0800

    针对a-l-d-p改了两句

--- app/[locale]/(default)/page.tsx
+++ app/[locale]/(default)/page.tsx
@@ -1,8 +1,9 @@
 import { getLandingPage } from "@/services/page"
 import PageContent from "./PageContent"
 
-export default async function Page({ params }: { params: { locale: string } }) {
-  const page = await getLandingPage(params.locale)
+export default async function Page({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params
+  const page = await getLandingPage(locale)
 
-  return <PageContent page={page} locale={params.locale} />
+  return <PageContent page={page} locale={locale} />
 }
